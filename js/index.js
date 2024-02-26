6be29e63456c4a55474980036450aeab87038cea
murtuzzov <murtuzzov>
2024-02-26 23:22:48 +0400

    first commit

---
+++ js/index.js
@@ -0,0 +1,79 @@
+// ЗАДАЧА No27
+// let number = 5;
+// for (let i = 1; i <= number; i += 1) {
+//   let result = i ** 2;
+//   console.log(result);
+// }
+
+// ЗАДАЧА No28
+// const number = 100;
+// let counter = 0;
+// while(counter<number){
+//  counter+=1;
+//  if(counter % 5 !== 0){
+//    continue;
+//  }
+//  console.log(counter)
+// }
+
+// ЗАДАЧА No29
+// let number = 3;
+// let sum;
+// for(let i = 1; i<=10; i+=1){
+//  sum = number * i;
+//   console.log(sum);
+// }
+
+// ЗАДАЧА No30
+// let number = 50;
+// let counter = 1;
+// while(counter<number){
+//  counter+=1;
+//  if(counter % 3 !== 0  counter % 5 !== 0){
+//    continue;
+//  }
+//   console.log(counter);
+// }
+
+// ЗАДАЧА No31
+// let number = 15;
+// let sum = 0;
+// for(let counter = 1; counter <= number; counter+=1){
+//  if(counter % 2 === 0){
+//    continue;
+//  }
+// sum+=counter;
+//   console.log(sum)
+// }
+
+// ЗАДАЧА No32
+// let number = 100;
+// let sum = 0;
+
+// for (let i = 0; i <= number; i += 1)
+//   if (i % 3 === 0 || i % 5 === 0) {
+//     sum += i;
+//   }
+
+// console.log(sum);
+
+//ЗАДАЧА No33
+// let number = 100;
+// let sum = 0;
+
+// for (let i = 1; i <= number; i += 1) {
+//   if (i % 7 === 0 && i % 5 !== 0) {
+//     console.log(i);
+//   }
+// }
+
+//Задача 34: Посчитать сумму всех цифр в числе 123456789.
+
+// let number = 123456789;
+// let strNum = number.toString();
+// let sum = 0;
+
+// for (let i = 0; i <= strNum.length; i += 1) {
+//   sum += i;
+//   console.log(sum);
+// }
